04942992e8397cb12e45b5e93b45b259e59aa6eb
Learn-Termux <91124025+Learn-Termux@users.noreply.github.com>
2021-09-21 19:36:14 +0530

    Add files via upload

---
+++ install-ngrok.sh
@@ -0,0 +1,54 @@
+#!bin/bash
+#colors
+#
+#
+#
+#
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+cyan='\033[36m'
+magneta='\033[1;35m'
+reset='\033[0m'
+#
+#
+#
+rm -rf $PREFIX/bin/ngrok
+
+pkg install wget proot resolv-conf -y
+
+
+case `dpkg --print-architecture` in
+aarch64)
+    arch="arm64" ;;
+arm)
+    arch="arm" ;;
+armhf)
+    arch="arm" ;;
+amd64)
+    arch="amd64" ;;
+i*86)
+    arch="386" ;;
+x86_64)
+    arch="amd64" ;;
+*)
+    echo -e  "$green Unsupported Architecture ..."
+esac
+
+
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-${arch}.zip -O ngrok.zip
+
+unzip ngrok.zip
+rm ngrok.zip
+mv ngrok $PREFIX/bin/
+
+
+echo
+echo -e "$green Sucessfully Installed ..."
+
+echo -e "$magneta login : ngrok authtoken <$yellow Your Authtoken $magneta >"
+echo -e "$reset"
+
+
+
+
